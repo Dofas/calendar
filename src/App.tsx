49c50b6d71a9c3fe2bd46ca057d3d56bd3e4e855
Dofas <dofafoga@gmail.com>
2021-09-17 01:14:04 +0300

    Added router for auth and no auth users

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import AppRouter from './Components/AppRouter';
+
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <AppRouter />
     </div>
   );
 }
